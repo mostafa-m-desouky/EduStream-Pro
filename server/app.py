40c2a6e35933c3d1413c2815475c5558d431d238
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2026-03-31 22:35:42 +0200

    Initial commit: Setup Flask with PostgreSQL and Models

---
+++ server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from models import db, User, Course, Lesson, Payment, Enrollment
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    return "LearnFlow API is Running!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
